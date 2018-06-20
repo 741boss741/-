edfa543b61a5a066ef17d92bee8f5562aeba21ca
xdrown <40439836+xdrown@users.noreply.github.com>
2018-06-20 23:08:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,43 @@
 const botSettings = require("./botsettings.json")
 const Discord = require("discord.js");
+const prefix = botSettings.prefix;
 
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on("ready", () =?> {
+bot.on("ready", async() => {
     console.log('Bot is ready! ${bot.user.username}');
+    
+    try {
+        Let link = await bot.generateInvite(["ADMINSTRATOR"]);
+        console.log(link);
+    } catch(e) {
+        console.log(e.stack);  
+    }
+});
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    Let messageArray = message.content.split(" ");
+    Let command = messageArray[0];
+    Let args = messageArray.slice(1);
+
+    if(!command.startsWith(prefix)) return;
+
+    if(command === `${prefix}userinfo`) {
+        Let embed = new Discord.RichEmbed();
+            .setAuthor(message.author.username)
+            .setDescription("This is The user's info!")
+            .setColor("RANDOM")
+            .addField("Full Username", `${message.author.username}#${message.author.discriminato}`)
+            .addField("ID", message.author.id)
+            .addField("Created At", message.author.createdAt)
+            .setFooter("This Dick Man Footer cool !")
+            .setThumbnail(message.author.avatarURL);
+
+        message.channel.sendEmbed(embed);
+    }
 });
 
 bot.login("botSettings.token");
