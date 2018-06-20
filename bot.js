4769797d17c64100ddde70ed24af722a10da148c
xdrown <40439836+xdrown@users.noreply.github.com>
2018-06-20 21:05:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'trabiza') {
+    	message.reply('pliardo ya kosomk');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
