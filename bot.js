0046bcd398c18f6086ae9806b9fb864786e8bad1
xdrown <40439836+xdrown@users.noreply.github.com>
2018-06-20 21:38:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+bot.on('message', message => {
+   var sender = message.author;
+   var msg = message.content.toUpperCase();
+   var prefix = '!>'
+   //Commands
+   if (msg === prefix + 'PING') {
+      message.channel.send('Pong!')
+   }
+});
+
 client.on('ready', () => {
     console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'trabiza') {
-    	message.reply('pliardo ya kosomk');
-  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
