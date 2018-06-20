4804ed3d4a61ced3e68c619e578e4bbbfd71f045
xdrown <40439836+xdrown@users.noreply.github.com>
2018-06-20 21:43:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-bot.on('message', message => {
-   var sender = message.author;
-   var msg = message.content.toUpperCase();
-   var prefix = '!>'
-   //Commands
-   if (msg === prefix + 'PING') {
-      message.channel.send('Pong!')
-   }
-});
-
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// THIS  MUST  BE  THIS  WAY
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+//THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
