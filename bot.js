b384c9dada92da06577bc55a2f59d9d363b797b7
xdrown <40439836+xdrown@users.noreply.github.com>
2018-06-20 23:15:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,15 @@
-const botSettings = require("./botsettings.json")
-const Discord = require("discord.js");
-const prefix = botSettings.prefix;
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const bot = new Discord.Client({disableEveryone: true});
-
-bot.on("ready", async() => {
-    console.log('Bot is ready! ${bot.user.username}');
-    
-    try {
-        Let link = await bot.generateInvite(["ADMINSTRATOR"]);
-        console.log(link);
-    } catch(e) {
-        console.log(e.stack);  
-    }
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-bot.on("message", async message => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-
-    Let messageArray = message.content.split(" ");
-    Let command = messageArray[0];
-    Let args = messageArray.slice(1);
-
-    if(!command.startsWith(prefix)) return;
-
-    if(command === `${prefix}userinfo`) {
-        Let embed = new Discord.RichEmbed();
-            .setAuthor(message.author.username)
-            .setDescription("This is The user's info!")
-            .setColor("RANDOM")
-            .addField("Full Username", `${message.author.username}#${message.author.discriminato}`)
-            .addField("ID", message.author.id)
-            .addField("Created At", message.author.createdAt)
-            .setFooter("This Dick Man Footer cool !")
-            .setThumbnail(message.author.avatarURL);
-
-        message.channel.sendEmbed(embed);
-    }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-bot.login("process.env.BOT_TOKEN");
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
