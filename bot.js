fe7e4315efa6964198cf3aa3f0c33eb05590e989
xdrown <40439836+xdrown@users.noreply.github.com>
2018-06-21 23:31:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on('ready', (DDDD) => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
