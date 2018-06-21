0c924329e6866a44bf32fa748e425c591fd9493d
xdrown <40439836+xdrown@users.noreply.github.com>
2018-06-21 22:26:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,8 @@ client.on('message', msg => {
   }
 });
 
+ client.user.setStatus('Hey Noob ?!', 'Watch Porn Now off !', 'to Help Type "!>help" haha !', 'Made by XDROWN') 
+
 client.on('ready',  () => {
   console.log('By : XDROWN#5950');
   console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
